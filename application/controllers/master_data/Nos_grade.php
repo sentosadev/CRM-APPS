19559282813cc11cefeebf25467152ee64647236
Husna <husna3305@gmail.com>
2021-05-19 16:57:12 +0700

    Master, create NOS Grade

---
+++ application/controllers/master_data/Nos_grade.php
@@ -0,0 +1,181 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class nos_grade extends Crm_Controller
+{
+  var $title  = "NOS Grade";
+
+  public function __construct()
+  {
+    parent::__construct();
+    if (!logged_in()) redirect('auth/login');
+    $this->load->model('nos_grade_model', 'nos_m');
+  }
+
+  public function index()
+  {
+    $data['title'] = $this->title;
+    $data['file']  = 'view';
+    $this->template_portal($data);
+  }
+
+  public function fetchData()
+  {
+    $fetch_data = $this->_makeQuery();
+    $data = array();
+    $user = user();
+    $no = $this->input->post('start') + 1;
+    foreach ($fetch_data as $rs) {
+      $params      = [
+        'get'   => "id = " . $rs->id_nos_grade
+      ];
+      $aktif = '';
+      if ($rs->aktif == 1) {
+        $aktif = '<i class="fa fa-check"></i>';
+      }
+
+      $sub_array   = array();
+      $sub_array[] = $no;
+      $sub_array[] = $rs->id_nos_grade;
+      $sub_array[] = $rs->nos_grade;
+      $sub_array[] = $aktif;
+      $sub_array[] = link_on_data_details($params, $user->id_user);
+      $data[]      = $sub_array;
+      $no++;
+    }
+    $output = array(
+      "draw"            => intval($_POST["draw"]),
+      "recordsFiltered" => $this->_makeQuery(true),
+      "data"            => $data
+    );
+    echo json_encode($output);
+  }
+
+  function _makeQuery($recordsFiltered = false)
+  {
+    $start  = $this->input->post('start');
+    $length = $this->input->post('length');
+    $limit  = "LIMIT $start, $length";
+    if ($recordsFiltered == true) $limit = '';
+
+    $filter = [
+      'limit'  => $limit,
+      'order'  => isset($_POST['order']) ? $_POST['order'] : '',
+      'search' => $this->input->post('search')['value'],
+      'order_column' => 'view',
+      'deleted' => false
+    ];
+    if ($recordsFiltered == true) {
+      return $this->nos_m->getMasterNOSGrade($filter)->num_rows();
+    } else {
+      return $this->nos_m->getMasterNOSGrade($filter)->result();
+    }
+  }
+
+  public function insert()
+  {
+    $data['title'] = $this->title;
+    $data['file']  = 'insert';
+    $this->template_portal($data);
+  }
+
+  public function saveData()
+  {
+    $user = user();
+    $post     = $this->input->post();
+
+    $insert = [
+      'nos_grade' => $post['nos_grade'],
+      'aktif'      => isset($_POST['aktif']) ? 1 : 0,
+      'created_at'    => waktu(),
+      'created_by' => $user->id_user,
+    ];
+
+    $tes = ['insert' => $insert];
+    // send_json($tes);
+    $this->db->trans_begin();
+    $this->db->insert('ms_nos_grade', $insert);
+    if ($this->db->trans_status() === FALSE) {
+      $this->db->trans_rollback();
+      $response = ['status' => 0, 'pesan' => 'Telah terjadi kesalahan !'];
+    } else {
+      $this->db->trans_commit();
+      $response = [
+        'status' => 1,
+        'url' => site_url(get_slug())
+      ];
+      $this->session->set_flashdata(msg_sukses_simpan());
+    }
+    send_json($response);
+  }
+
+  public function edit()
+  {
+    $data['title'] = $this->title;
+    $data['file']  = 'edit';
+    $filter['id_nos_grade']  = $this->input->get('id');
+    $row = $this->nos_m->getMasterNOSGrade($filter)->row();
+    if ($row != NULL) {
+      $data['row'] = $row;
+      $this->template_portal($data);
+    } else {
+      $this->session->set_flashdata(msg_not_found());
+      redirect(get_slug());
+    }
+  }
+
+  public function saveEdit()
+  {
+    $user = user();
+    $post     = $this->input->post();
+    $fg = ['id_nos_grade' => $post['id_nos_grade']];
+    $gr = $this->nos_m->getMasterNOSGrade($fg)->row();
+
+    //Cek Data
+    if ($gr == NULL) {
+      $result = [
+        'status' => 0,
+        'pesan' => 'Data tidak ditemukan '
+      ];
+      send_json($result);
+    }
+
+    $update = [
+      'nos_grade' => $post['nos_grade'],
+      'aktif'           => isset($_POST['aktif']) ? 1 : 0,
+      'updated_at'      => waktu(),
+      'updated_by'      => $user->id_user,
+    ];
+
+    // $tes = ['update' => $update];
+    // send_json($tes);
+    $this->db->trans_begin();
+    $this->db->update('ms_nos_grade', $update, $fg);
+    if ($this->db->trans_status() === FALSE) {
+      $this->db->trans_rollback();
+      $response = ['status' => 0, 'pesan' => 'Telah terjadi kesalahan !'];
+    } else {
+      $this->db->trans_commit();
+      $response = [
+        'status' => 1,
+        'url' => site_url(get_slug())
+      ];
+      $this->session->set_flashdata(msg_sukses_update());
+    }
+    send_json($response);
+  }
+
+  public function detail()
+  {
+    $data['title'] = $this->title;
+    $data['file']  = 'detail';
+    $filter['id_nos_grade']  = $this->input->get('id');
+    $row = $this->nos_m->getMasterNOSGrade($filter)->row();
+    if ($row != NULL) {
+      $data['row'] = $row;
+      $this->template_portal($data);
+    } else {
+      $this->session->set_flashdata(msg_not_found());
+      redirect(get_slug());
+    }
+  }
+}
