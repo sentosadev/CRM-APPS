cb183e72d39682122e000777e4ec2b23a97ad2d0
Husna <husna3305@gmail.com>
2021-06-16 08:29:51 +0700

    API4, add cek stage on Stage 10, create Stage 12

--- application/controllers/api/crm_api/mdms/v1/Follow_up.php
+++ application/controllers/api/crm_api/mdms/v1/Follow_up.php
@@ -146,13 +146,21 @@ class Follow_up extends CI_Controller
       $leads_id = $ld->leads_id;
       $leads = $post['leads'];
 
-      //Cek StageId 8
-      $lstg = [
-        'leads_id' => $leads_id,
-        'stageId' => 8
-      ];
-      $cek_stage = $this->ld_m->getLeadsStage($lstg)->row();
-      if ($cek_stage != NULL) {
+      //Cek StageId 1,4,5,8
+      $list_cek_stage = [1, 4, 5, 8];
+      $stage_belum = [];
+      foreach ($list_cek_stage as $vs) {
+        $lstg = [
+          'leads_id' => $leads_id,
+          'stageId' => $vs
+        ];
+        $cek_stage = $this->ld_m->getLeadsStage($lstg)->row();
+        if ($cek_stage == NULL) {
+          $stage_belum[] = $vs;
+        }
+      }
+
+      if (count($stage_belum) == 0) {
         //History Stage ID
         $insert_history_stage = [
           'leads_id' => $leads_id,
@@ -209,7 +217,8 @@ class Follow_up extends CI_Controller
         }
       } else {
         $status = 0;
-        $message = 'Stage 8 belum diperoses';
+        $stage = implode(', ', $stage_belum);
+        $message = "Stage $stage belum diperoses";
       }
     } else {
       $status = 0;
@@ -251,28 +260,92 @@ class Follow_up extends CI_Controller
   {
     // 12. Create Sales Order (after SSU)
     //Belum Ada Pengecekan StageId 8
-    $fc = ['idProspek' => $post['idProspek']];
+    $fc = ['idSPK' => $post['idSPK']];
     $ld = $this->ld_m->getLeads($fc)->row();
-    $update = [
-      'followUpKe' => $ld->jumlahFollowUp + 1,
-      'idSPK' => $this->input->post('idSPK', true),
-      'kodeTypeUnitDeal' => $this->input->post('kodeTypeUnitDeal', true),
-      'kodeWarnaUnitDeal' => $this->input->post('kodeWarnaUnitDeal', true),
-      'deskripsiPromoDeal' => $this->input->post('deskripsiPromoDeal', true),
-      'metodePembayaranDeal' => $this->input->post('metodePembayaranDeal', true),
-      'kodeLeasingDeal' => $this->input->post('kodeLeasingDeal', true),
-      'frameNo' => $this->input->post('frameNo', true),
-      'stageId_12_processed_at'    => waktu(),
-      'stageId_12_processed_by_user_d_nms' => $post['id_user'],
-    ];
-    $this->db->trans_begin();
-    $cond = ['leads_id' => $ld->leads_id];
-    $this->db->update('leads', $update, $cond);
-    if ($this->db->trans_status() === FALSE) {
-      $this->db->trans_rollback();
+    $status = 1;
+    $message = '';
+    if ($ld != NULL) {
+      $leads_id = $ld->leads_id;
+      $leads = $post['leads'];
+
+      //Cek StageId 1,4,5,8,10
+      $list_cek_stage = [1, 4, 5, 8, 10];
+      $stage_belum = [];
+      foreach ($list_cek_stage as $vs) {
+        $lstg = [
+          'leads_id' => $leads_id,
+          'stageId' => $vs
+        ];
+        $cek_stage = $this->ld_m->getLeadsStage($lstg)->row();
+        if ($cek_stage == NULL) {
+          $stage_belum[] = $vs;
+        }
+      }
+
+      if (count($stage_belum) == 0) {
+        //History Stage ID
+        $insert_history_stage = [
+          'leads_id'   => $leads_id,
+          'stageId'    => $post['stageId'],
+          'created_at' => waktu(),
+          'created_by' => $leads['id_user'],
+        ];
+      }
+
+      $update_leads = [
+        'idSPK'                   => $leads['idSPK'],
+        'kodeTypeUnitDeal'        => $leads['kodeTypeUnitDeal'],
+        'kodeWarnaUnitDeal'       => $leads['kodeWarnaUnitDeal'],
+        'deskripsiPromoDeal'      => $leads['deskripsiPromoDeal'],
+        'metodePembayaranDeal'    => $leads['metodePembayaranDeal'],
+        'kodeLeasingDeal'         => $leads['kodeLeasingDeal'],
+        'stageId_10_processed_at' => waktu(),
+        'stageId_10_processed_by_user_d_nms' => $leads['id_user'],
+      ];
+
+      //Cek Last FollowUp
+      $ffol = [
+        'leads_id' => $leads_id,
+        'assignedDealer' => $ld->assignedDealer,
+        'select' => 'count'
+      ];
+      $count_fol = $this->ld_m->getLeadsFollowUp($ffol)->row()->count;
+      $ins_fol_up = [
+        'followUpID' => $this->ld_m->getFollowUpID(),
+        'followUpKe' => $count_fol + 1,
+        'pic' => $post['fol_up']['pic'],
+        'tglFollowUp' => $post['fol_up']['tglFollowUp'],
+        'kodeHasilStatusFollowUp' => $post['fol_up']['kodeHasilStatusFollowUp'],
+        'id_status_fu' => $post['fol_up']['id_status_fu'],
+        'created_at' => waktu()
+      ];
+
+      $tes = [
+        'ins_fol_up' => $ins_fol_up,
+        'update_leads' => $update_leads,
+        'insert_history_stage' => isset($insert_history_stage) ? $insert_history_stage : ''
+      ];
+      // send_json($tes);
+      if (isset($ins_leads_history_stage)) {
+        $this->db->trans_begin();
+        $cond = ['leads_id' => $leads_id];
+        $this->db->insert('leads_follow_up', $ins_fol_up);
+        $this->db->update('sleads', $update_leads, $cond);
+        if ($this->db->trans_status() === FALSE) {
+          $this->db->trans_rollback();
+        } else {
+          $this->db->trans_commit();
+          $status = 1;
+        }
+      } else {
+        $status = 0;
+        $stage = implode(', ', $stage_belum);
+        $message = "Stage $stage belum diperoses";
+      }
     } else {
-      $this->db->trans_commit();
-      return true;
+      $status = 0;
+      $message = 'Leads ID tidak ditemukan';
     }
+    return ['status' => $status, 'message' => $message];
   }
 }
